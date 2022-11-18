682ff8e7cb281f88a639f6da756d3f792416220d
jschuyl <schuylertjed@gmail.com>
2022-11-17 19:08:06 -0700

    got tables successfully seeded, now onto manipulation wiithin the js file? there might be more, but that seems like the next logical step

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,21 @@
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES  ("Jedediah", "Schuyler"),
-        ("Mark", "Brown"),
-        ("Jenni", "Thomas"),
-        ("Benjamin", "Button"),
-        ("Samuel", "Higgins");
+INSERT INTO department (name)
+VALUES  ("Management"),
+        ("Softgoods"),
+        ("Hardgoods"),
+        ("Shipping & Receiving"),
+        ("Action Sports");
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES ()
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Manager", 80000, 1),
+        ("Sales Specialist", 25000, 2),
+        ("Sales Specialist", 25000, 3),
+        ("Shipper Receiver", 30000, 4),
+        ("Advisor", 30000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("Jenni", "Thomas", 1, NULL),
+        ("Jedediah", "Schuyler", 4, 1),
+        ("Mark", "Brown", 3, 1),
+        ("Benjamin", "Button", 2, 1),
+        ("Samuel", "Higgins", 5, 1);
+
