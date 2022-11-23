dda5c51830133e1fcc24cae421295aabbee719e4
jschuyl <schuylertjed@gmail.com>
2022-11-22 20:18:30 -0700

    FULL FUNCTIONALITY

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const ctable = require('console.table'); // `C` table... get it?
-const { response } = require('express');
-
-require('dotenv').config()
 
 const connection = mysql.createConnection(
   {
@@ -109,9 +106,9 @@ function viewRoles(){ // FINISHED
 
 function viewEmployees(){ // FINISHED
 
-  const tellMeById = `SELECT employee.id, employee.first_name, employee.last_name, department.name, role.title, role.salary FROM employee LEFT JOIN role ON role.id=employee.role_id LEFT JOIN department ON department.id=role.department_id;`;
-  const tellMeByLastName = `SELECT employee.id, employee.last_name, employee.first_name, department.name, role.title, role.salary FROM employee LEFT JOIN role ON role.id=employee.role_id LEFT JOIN department ON department.id=role.department_id ORDER BY last_name ASC;`;
-  const tellMeByFirstName = `SELECT employee.id, employee.first_name, employee.last_name, department.name, role.title, role.salary FROM employee LEFT JOIN role ON role.id=employee.role_id LEFT JOIN department ON department.id=role.department_id ORDER BY first_name ASC;`
+  const tellMeById = `SELECT employee.id, employee.first_name, employee.last_name, department.name AS department_name, role.title, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager_name FROM employee LEFT JOIN role ON role.id=employee.role_id LEFT JOIN department ON department.id=role.department_id LEFT JOIN employee manager ON employee.manager_id=manager.id;`;
+  const tellMeByLastName = `SELECT employee.id, employee.last_name, employee.first_name, department.name AS department_name, role.title, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager_name FROM employee LEFT JOIN role ON role.id=employee.role_id LEFT JOIN department ON department.id=role.department_id  LEFT JOIN employee manager ON employee.manager_id=manager.id ORDER BY last_name ASC;`;
+  const tellMeByFirstName = `SELECT employee.id, employee.first_name, employee.last_name, department.name AS department_name, role.title, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager_name FROM employee LEFT JOIN role ON role.id=employee.role_id LEFT JOIN department ON department.id=role.department_id LEFT JOIN employee manager ON employee.manager_id=manager.id ORDER BY first_name ASC;`
   // Narrow by user known information
   inquirer
     .prompt([
@@ -242,16 +239,20 @@ connection.query('SELECT * FROM department', (err, depInfo) => {
   })
 };
 
-function addRole(){ // NEEDS TO BE STARTED
-  inquirer
-    .prompt([
+function addRole(){ // NEEDS TESTING
+  let chooseDepartments = [];
+
+  connection.query('SELECT * FROM department', (err, depInfo) => {
+    if (err) throw err;
+     chooseDepartments = depInfo.map(({ id, name })=> ({ name: name, value: id })); 
+  
+  
+    inquirer.prompt([
       {
-        type: `input`,
+        type: `list`,
         message: `Which department will this new role be in?`,
         name: `department`,
-        choices: [
-          // see if you can populate the department names from department table mySQL as choices
-        ]        
+        choices: chooseDepartments
       },
       {
         type: `input`,
@@ -263,13 +264,21 @@ function addRole(){ // NEEDS TO BE STARTED
         message: `What is the yearly salary of this new role?`,
         name: `dollaDollaBills`
       }
-
     ])
+    .then((response) => {
+      connection.query(`INSERT INTO roles (title, salary, department_id) VALUES (${response.title}, ${response.dollaDollaBills}, ${response.department})`, (err) => {
+        if (err) throw err;
+        console.log(`${response.title} role added to ${response.department}`);
+        questions();
+      })
+    })
 
-}
+})}
 
 function updateEmployee(){ // NEEDS WORK
-  
+  let thisHereEmployee = [];
+  let thisHereEmployeeId = '';
+
   inquirer
     .prompt([
       {
@@ -279,25 +288,143 @@ function updateEmployee(){ // NEEDS WORK
       }
     ])
       .then((response) => {
-        const selectEmployee = `SELECT employee.id, employee.first_name, employee.last_name, department.name, role.title, role.salary FROM employee LEFT JOIN role ON role.id=employee.role_id LEFT JOIN department ON department.id=role.department_id WHERE employee.id=${response.Id};`
+        
+        const selectEmployee = `SELECT employee.id, employee.first_name, employee.last_name, department.name, role.title, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager_name FROM employee LEFT JOIN role ON role.id=employee.role_id LEFT JOIN department ON department.id=role.department_id LEFT JOIN employee manager ON employee.manager_id=manager.id WHERE employee.id=${response.Id};`
 
-        connection.query(selectEmployee, (err, rows) =>{
+        thisHereEmployeeId = response.Id;
+
+        connection.query(selectEmployee, (err, empInfo) =>{
           if (err) throw err;
-          console.table(rows)
-        })
-      })
-    /*
-      {
-        type: `list`,
-        message: `What would you like to update for this employee?`, // might be zesty to get the employee's name in there?
-        choices: [
-          `Update first name`,
-          `Update last name`,
-          `Update role`,
-          `Terminate` // hoping this isn't over acheiving
-        ],
-        name: `inMyHands`
-      }*/
-    
-  
-  }
+          thisHereEmployee = empInfo.map(({ id, first_name, last_name, title, manager_name}) => ({ name: `${first_name} ${last_name}`, value: id},
+          console.log(`${first_name} ${last_name} works as a ${title} under ${manager_name}`)));
+          
+
+          inquirer
+          .prompt([
+            {
+              type: `list`,
+              message: `What would you like to update for ${thisHereEmployee.name}?`,
+              choices: [
+                `Update first name`,
+                `Update last name`,
+                `Update role`,
+                `Terminate` // hoping this isn't over acheiving
+              ],
+              name: `inMyHands`
+            }
+          ])
+          .then((response) => {
+            switch (response.inMyHands) {
+              case 'Update first name':
+                inquirer
+                  .prompt([
+                    {
+                      type: 'input',
+                      message: 'What would you like to change their first name to?',
+                      name: 'newFirstName'
+                    }
+                  ])
+                  .then((response) => {
+                    connection.query(`UPDATE employee SET first_name='${response.newFirstName}' WHERE employee.id=${thisHereEmployeeId}`, (err, result) => {
+                      if (err) throw err;
+                      console.log(`Employee #${thisHereEmployeeId} had their first name successfully changed to ${response.newFirstName}`);
+                      questions();
+                    })})
+                    break;
+                case 'Update last name':
+                  inquirer
+                  .prompt([
+                    {
+                      type: 'input',
+                      message: 'What would you like to change their last name to?',
+                      name: 'newLastName'
+                    }
+                  ])
+                  .then((response) => {
+                    connection.query(`UPDATE employee SET last_name='${response.newLastName}' WHERE employee.id=${thisHereEmployeeId}`, (err, result) => {
+                      if (err) throw err;
+                      console.log(`Employee #${thisHereEmployeeId} had their Last name successfully changed to ${response.newLastName}`);
+                      questions();
+                    })})
+                    break; 
+                case 'Update role':
+                  let chooseDepartments = [];
+                  let chooseRoles = [];
+                  let chooseManagers = [];
+
+                  // GET DEPARTMENT NAMES
+                  connection.query('SELECT * FROM department', (err, depInfo) => {
+                    if (err) throw err;
+                    chooseDepartments = depInfo.map(({ id, name })=> ({ name: name, value: id })); 
+
+
+                    inquirer.prompt([
+                      {
+                        type: `list`,
+                        message: `Which department will this employee now work in?`,
+                        name: `department`,
+                        choices: chooseDepartments
+                      },
+                   ])
+                    .then((response) => {
+                      // GET ROLE NAMES
+                      connection.query(`SELECT role.id, role.title FROM role WHERE department_id=${response.department}`, (err, roleInfo) => {
+                      if (err) throw err;
+                      chooseRoles = roleInfo.map(({ id, title}) => ({name: title, value: id}));
+                      // GET MANAGER NAMES
+                      connection.query(`SELECT employee.id, employee.first_name, employee.last_name FROM employee WHERE role_id=1`, (err, managerInfo) => {
+                        if (err) throw err;
+                        chooseManagers = managerInfo.map(({ id, first_name, last_name}) => ({name: `${first_name} ${last_name}`, value: id }));
+
+                        inquirer
+                        .prompt([
+                          {
+                            type: `list`,
+                            message: `What is their new role title?`,
+                            name: `title`,
+                            choices: chooseRoles
+                          },
+                          {
+                            type: `list`,
+                            message: `Who will manage this employee`,
+                            name: 'micro',
+                            choices: chooseManagers
+                          }
+                        ])
+                        .then((response) => {
+                          connection.query(`UPDATE employee SET role_id=${response.title}, manager_id=${response.micro} WHERE employee.id=${thisHereEmployeeId}`, (err) => {
+                            console.log(`Employee updated`)
+                            if (err) throw err;
+                            questions();
+
+                              })
+                            })
+                          })
+                        })
+                      })
+                    }) 
+                    break;
+                  case 'Terminate':
+                     inquirer
+                     .prompt([
+                      {
+                        type: 'confirm',
+                        message: 'This action is irreversible, are you sure you wish to continue?',
+                        name: 'emperorsThumb'
+                      }
+                     ])
+                     .then((response) => {
+                        if (response.emperorsThumb === true ){
+                          connection.query(`DELETE FROM employee WHERE employee.id=${thisHereEmployeeId}`, (err) => {
+                            console.log(`Employee #${thisHereEmployeeId} terminated, returning to menu`);
+                            questions();
+                          })
+                        } if (response.emperorsThumb === false) {
+                          console.log('Termination aborted, returning to menu');
+                          questions();
+                        }
+                     })
+                     break;              
+                  }
+                })
+              })})}
